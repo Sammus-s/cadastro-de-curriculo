23010b7e0d773634b9625e4a215457d16a0ba9fc
Sam <samuel.rodrigues2004@hotmail.com>
2024-11-29 08:02:23 -0300

    fix: romoved old screens from router

--- resume-frontend/src/app/routes/router.tsx
+++ resume-frontend/src/app/routes/router.tsx
@@ -1,13 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../../App";
 import Home from "./Home";
-import SobreNos from "./SobreNos";
-import Agricultura from "./Agricultura";
-import Avicultura from "./Avicultura";
-import Suinocultura from "./Suinocultura";
-import Bovinocultura from "./Bovinocultura";
-import Administrativo from "./Administrativo";
-import Contato from "./Contato";
 import TrabalheConosco from "./TrabalheConosco";
 
 export const router = createBrowserRouter([
@@ -19,35 +12,7 @@ export const router = createBrowserRouter([
         {
           path: '/',
           element: <Home />
-        },
-        {
-          path: 'sobre-nos',
-          element: <SobreNos />
-        },
-            {
-              path: 'solucoes/agricultura',
-              element: <Agricultura />
-            },
-            {
-              path: 'solucoes/avicultura',
-              element: <Avicultura />
-            },
-            {
-              path: 'solucoes/suinocultura',
-              element: <Suinocultura />
-            },
-            {
-              path: 'solucoes/bovinocultura',
-              element: <Bovinocultura />
-            },
-            {
-              path: 'solucoes/administrativo',
-              element: <Administrativo />
-            },
-        {
-          path: 'contato/',
-          element: <Contato />
-        },     
+        },  
             {
               path: 'contato/trabalhe-conosco',
               element: <TrabalheConosco />
